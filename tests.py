3e0dfa8d5a6cefb8203febf3c5c771371cacd6f6
Gene Boggs <gene@ology.net>
2025-09-13 21:52:29 -0700

    WIP

--- tests.py
+++ tests.py
@@ -34,6 +34,8 @@ class TestMusicCreatingRhythms(unittest.TestCase):
         self.assertEqual(self.mcr.cfsqrt(2), [1, 2])
         self.assertEqual(self.mcr.cfsqrt(3), [1, 1, 2])
         self.assertEqual(self.mcr.cfsqrt(4), [2])
+        self.assertEqual(self.mcr.cfsqrt(5), [2, 4])
+        self.assertEqual(self.mcr.cfsqrt(6), [2, 2, 4])
         self.assertEqual(self.mcr.cfsqrt(42), [6, 2, 12])
 
     # def test_chsequl(self):
